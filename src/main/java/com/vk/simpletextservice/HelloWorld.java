e4c1abc1294bae3f14761e2db7cd6778f4ecf7f8
Vladimir Karakashev <vladimir.karakashev@gmail.com>
2021-05-29 19:35:41 +0300

    add Module B

--- src/main/java/com/vk/simpletextservice/HelloWorld.java
+++ src/main/java/com/vk/simpletextservice/HelloWorld.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorld {
 
-    @RequestMapping("/text")
+    @RequestMapping("/")
     public @ResponseBody String helloText() {
         return "Hello World!";
     }
